dc8d324db4450c222f2b9cb98e943305b50dc7b4
ComputerGuy <48910590+vicmee76@users.noreply.github.com>
2026-09-19 03:37:23 +0100

    Update Services.tsx

--- src/components/sections/Services.tsx
+++ src/components/sections/Services.tsx
@@ -233,6 +233,8 @@ export function Services() {
               </p>
             </div>
 
+            
+
             {/* Oversized index, ghosted into the right half of the panel. */}
             {services.map((service, index) => (
               <GhostIndex key={service.title} progress={scrollYProgress} index={index} />
